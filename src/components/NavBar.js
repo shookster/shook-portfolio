ccf6a42bc5e16bdcb35d79e1e20aa8daa104f9d4
Ethan Shook <eshook89@gmail.com>
2021-01-27 22:17:12 -0600

    added styling for name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { NavLink } from "react-router-dom";
 
-export default function Navbar() {
+export default function NavBar() {
     return (
-        <header>
-            <div>
-                <nav>
-                    <NavLink to="/" exact>
+        <header className="bg-red-600">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink to="/" exact 
+                    activeClassName="text-white"
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
                         Ethan
                     </NavLink>
                     <NavLink to="/post">
