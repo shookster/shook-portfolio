cb76b56b9f50e7cd2d718f4f34809442c0cbb1ae
Ethan Shook <eshook89@gmail.com>
2021-02-01 20:16:38 -0600

    added classes in navlink pt 2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,13 +11,13 @@ export default function NavBar() {
                     className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
                         Ethan
                     </NavLink>
-                    <NavLink to="/post">
+                    <NavLink to="/post" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
                         Blog Post
                     </NavLink>
-                    <NavLink to="/project">
+                    <NavLink to="/project" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
                         Projects
                     </NavLink>
-                    <NavLink to="/about">
+                    <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
                         About Me
                     </NavLink>
                 </nav>
