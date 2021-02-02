ff7ffc4f482719d9bb83bf28e00d34d326201e9a
Ethan Shook <eshook89@gmail.com>
2021-02-01 20:50:06 -0600

    fixed my oops. now the background image actually loads

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
-import image from "..background-brick-wall.jpeg"
+import image from "../background-brick-wall.jpg";
 
 export default function Home() {
     return (
         <main>
             <img src={image} alt="brick wall" className="absolute object-cover w-full h-full"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm Ethan.</h1>
+                <h1 className="text-6xl text-purple-100 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm Ethan.</h1>
             </section>
         </main>
     )
