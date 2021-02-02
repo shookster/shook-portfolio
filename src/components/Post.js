09c7c92457a31d9c54f5c1ffee662f3edf3f9bf6
Ethan Shook <eshook89@gmail.com>
2021-02-01 22:02:58 -0600

    added sanity stuff and first blog post

--- src/components/Post.js
+++ src/components/Post.js
@@ -23,21 +23,28 @@ export default function Post() {
     }, []);
 
     return (
-        <main>
-            <section>
-                <h1>Blog Posts Page</h1>
-                <h2>Projects and Articles I've Written</h2>
-                <div>
+        <main className="bg-green-100 min-h-screen p-12">
+            <section className="container mx-auto">
+                <h1 className="text-5xl flex justify-center cursive">Blog Posts Page</h1>
+                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Projects and Articles I've Written</h2>
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {postData && postData.map((post, index) => (
                     <article>
-                        <Link>
-                        <span>
-                            <img />
-                            <span>
-                                <h3></h3>
+                        <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                        <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
+                        key={index}>
+                            <img 
+                            src={post.mainImage.asset.url}
+                            alt={post.mainImage.alt}
+                            className="w-full h-full rounded-r object-cover absolute"
+                            />
+                            <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
+                                <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">{post.title}</h3>
                             </span>
                         </span>
                         </Link>
                     </article>
+))}
                 </div>
             </section>
         </main>
