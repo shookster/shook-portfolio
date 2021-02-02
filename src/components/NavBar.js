3dd38b1a6ab0a8c9f32200131153c7c1fb3c491e
Ethan Shook <eshook89@gmail.com>
2021-02-01 20:29:58 -0600

    added social buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons"
 
 export default function NavBar() {
     return (
@@ -11,16 +12,24 @@ export default function NavBar() {
                     className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
                         Ethan
                     </NavLink>
-                    <NavLink to="/post" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                    <NavLink to="/post"
+                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700"
+                    >
                         Blog Post
                     </NavLink>
-                    <NavLink to="/project" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                    <NavLink to="/project" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800" activeClassName="text-red-100 bg-red-700">
                         Projects
                     </NavLink>
-                    <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                    <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800" activeClassName="text-red-100 bg-red-700">
                         About Me
                     </NavLink>
                 </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon url="https://twitter.com/ethanshook" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35 , width: 35}}/>
+                    <SocialIcon url="https://github.com/shookster" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35 , width: 35}}/>
+                    <SocialIcon url="https://linkedin.com/in/ethanshook" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35 , width: 35}}/>
+                </div>
             </div>
         </header>
         )
