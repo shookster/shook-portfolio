f80f772372ddcf0eea1866c07dbcc96993f2c1f4
Ethan Shook <eshook89@gmail.com>
2021-02-02 08:56:05 -0600

    fixed background image error

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,10 @@ import image from "../background-brick-wall2.jpg";
 export default function Home() {
     return (
         <main>
-            <img src={image} alt="brick wall" className="absolute object-cover w-full h-full"/>
-            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-gray-800 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm Ethan.</h1>
-            </section>
+            <img src={image} alt="brick wall" className="absolute object-cover min-w-full min-h-full"/>
+                <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                    <h1 className="justified-center text-6xl text-gray-800 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm Ethan.</h1>
+                </section>
         </main>
-    )
+    );
 }
