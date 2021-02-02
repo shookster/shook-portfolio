cd03bdd7c6a76cc0c4c42055c2178868e9b1fd9c
Ethan Shook <eshook89@gmail.com>
2021-02-01 21:22:48 -0600

    added data request for post page

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,45 @@
-import React from "react"
+import React, {useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import sanityClient from "../client.js";
 
 export default function Post() {
-    return <h1>Post Page</h1>
+    const [postData, setPost] = useState(null);
+
+    useEffect(() => {
+        sanityClient
+            .fetch(`*[_type == "post"]{
+                title,
+                slug,
+                mainImage{
+                    asset->{
+                        _id,
+                        url
+                    },
+                    alt
+                }
+            }`)
+            .then((data) => setPost(data))
+            .catch(console.error);
+    }, []);
+
+    return (
+        <main>
+            <section>
+                <h1>Blog Posts Page</h1>
+                <h2>Projects and Articles I've Written</h2>
+                <div>
+                    <article>
+                        <Link>
+                        <span>
+                            <img />
+                            <span>
+                                <h3></h3>
+                            </span>
+                        </span>
+                        </Link>
+                    </article>
+                </div>
+            </section>
+        </main>
+    )
 }
